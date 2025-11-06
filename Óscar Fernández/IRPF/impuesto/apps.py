3b3480b24e6671d0ff1048498d2c1df082e393ca
oscarft06 <oscarft006@gmail.com>
2025-11-06 20:38:06 +0100

    Delete Óscar Fernández/IRPF directory

--- Óscar Fernández/IRPF/impuesto/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ImpuestoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'impuesto'
--- Óscar Fernández/IRPF/impuesto/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.declarar_irpf, name='declarar_irpf'),
-    path('resultado/<int:pk>/', views.resultado_irpf, name='resultado_irpf'),
-]
